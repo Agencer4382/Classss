7736ad4311b33f563974071dc11548e9d622e2ba
Agencer4382 <asimgencer4382@gmail.com>
2019-04-06 21:20:14 -0400

    April6 Class

--- src/com/April4/Greetings.java
+++ src/com/April4/Greetings.java
@@ -10,11 +10,13 @@ public class Greetings {
 		obj.findLargest(30, 31);//calling method FindLargest and passing values as 30&31
 		obj.findLargest(2000, 664674);
 		obj.helloToInstructor("ali", "veli");
-		
+		obj.findLargest(60, 50);
+		int ttl=obj.sum(4, 9);
+		System.out.println(ttl);
 	}
 	
 	void helloToInstructor(String a, String b) {//name="Awet"
-		System.out.println("Hello "+a+"\n"+b);
+		System.out.println("Hello "+a+"\nHello "+b);
 	}
 
 	void hello() {
@@ -27,7 +29,13 @@ public class Greetings {
 		}else {
 			System.out.println("B is larger than A");
 		}
+		
+		
 
+	}
+	int sum(int a,int b) {
+		int result=a+b;
+		return result;
 	}
 
 }
--- src/com/April4/Task1.java
+++ src/com/April4/Task1.java
@@ -4,20 +4,32 @@ public class Task1 {
 
 	public static void main(String[] args) {
 		Task1 num = new Task1();
-		num.evenOdd(7);
+		num.evenOdd(8);
+		num.isPalindrome("kabak");
 
 	}
 
 	void evenOdd(int a) {
 
 		if (a % 2 == 0) {
-			System.out.println(a+" is even");
+			System.out.println(a + " is even");
 		} else {
+			System.out.println(a + " is odd");
 
-		}System.out.println(a+" is odd");
-		
-		
-		
+		}
+
 	}
 
+	void isPalindrome(String str) {
+		String rev = "";
+
+		for (int i = str.length() - 1; i >= 0; i--) {
+			rev += str.charAt(i);
+		}
+		if (str.equals(rev)) {
+			System.out.println(str + " is a palindrome");
+		} else {
+			System.out.println(str + " is not a palindrome");
+		}
+	}
 }
